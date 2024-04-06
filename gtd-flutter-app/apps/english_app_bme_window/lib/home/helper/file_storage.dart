d404389971c359fbfa9dec9be0ab7b748cdb0169
duynt <maxpowerak@gmail.com>
2024-04-06 20:01:26 +0700

    update windows

--- gtd-flutter-app/apps/english_app_bme_window/lib/home/helper/file_storage.dart
+++ gtd-flutter-app/apps/english_app_bme_window/lib/home/helper/file_storage.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
 // To save the file in the device
 class FileStorage {
   static Future<String> getExternalDocumentPath() async {
     // To check whether permission is given for this app or not.
-    var status = await Permission.storage.status;
-    if (!status.isGranted) {
-      // If not we will ask for permission first
-      await Permission.storage.request();
-    }
+    // var status = await Permission.storage.status;
+    // if (!status.isGranted) {
+    //   // If not we will ask for permission first
+    //   await Permission.storage.request();
+    // }
     Directory directory = Directory("");
     if (Platform.isAndroid) {
       // Redirects it to download folder in android
